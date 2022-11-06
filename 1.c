c73b690addd1d3cd41a5f0ed49ed2e00b1372199
sucumbap <afonso@bash.pt>
2022-11-06 02:18:24 +0000

    May have completed 1.1 and simplified 1.2

--- 1.c
+++ 1.c
@@ -6,9 +6,7 @@ int wordPrint( char *word, void *context );
 
 int main () {
    char str[] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla suscipit lorem sit amet libero interdum dictum. Nulla vestibulum, mi egestas tincidunt rutrum, leo quam rutrum nibh, id pellentesque lorem libero sit amet est. Donec mattis tellus nibh, sit amet pharetra purus bibendum eget. Morbi bibendum vitae magna vitae dapibus. Nunc tempor cursus diam eu congue. Curabitur ac nulla et enim bibendum venenatis ut et risus. Nullam eu velit in est egestas aliquet. Nam vitae eleifend erat.";
-   int *proc = &wordPrint;
-   wordSplit(str, *(int*)proc(*str) , str);
-   
+   wordSplit( str, wordPrint, NULL );
    return(0);
 }
 
@@ -17,18 +15,20 @@ int wordSplit( char *str, int (*proc)( char *word, void *context ),void *context
 
    const char s[2] = " ";
    char *token;
+   int wordCounter = 0;
    
    /* get the first token */
    token = strtok(str, s);
    
    /* walk through other tokens */
    while( token != NULL ) {
-      printf( "%s\n", token );
-    
+      wordCounter += proc(token, context);
       token = strtok(NULL, s);
    }
-
+   printf("Total number of words: %d:", wordCounter);
+   return wordCounter;
 }
 int wordPrint( char *word, void *context ){
-   
+   printf( "%s\n", word );
+   return 1;
 }
